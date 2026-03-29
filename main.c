953e6ea5664d4b064c03f9932fdbd6454f995397
Muzzikon <i.stepanov2@g.nsu.ru>
2026-03-29 16:06:44 +0700

    Добавлена базовая обработка аргументов командной строки

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello, World!");
+int main(int argc, char *argv[]) {
+	if (argc < 3) {
+		printf("Usage: %s <file> <operation>\n", argv[0]);
+		printf("Example: %s data.csv -kd_nearest\n", argv[0]);
+		return 0;
+	}
+
+	printf("Input file: %s\n", argv[1]);
+	printf("Operation: %s\n", argv[2]);
+
 	return 0;
 }
