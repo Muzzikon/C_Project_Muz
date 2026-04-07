be0c784d816f01193f982b2d856b527deeec8863
Muzzikon <i.stepanov2@g.nsu.ru>
2026-04-07 23:49:56 +0700

    Добавил PointArray и прототипы brute force для работы с точками в памяти

--- include/kd_tree.h
+++ include/kd_tree.h
@@ -5,6 +5,11 @@ typedef struct Point {
     double x, y, z;  // Для 3D-данных
 } Point;
 
+typedef struct {
+    Point *points;
+    int count;
+} PointArray;
+
 typedef struct Node {
     Point point;
     struct Node *left, *right;
@@ -16,5 +21,8 @@ void print_tree(Node* root);
 Point nearest_neighbor(Node* root, Point target, int depth);
 void range_query(Node* root, Point lower, Point upper, int depth, Point* result, int* count);
 void free_tree(Node* root);
+
+Point brute_force_nearest(Point *points, int count, Point target);
+int brute_force_range(Point *points, int count, Point lower, Point upper, Point *result);
 
 #endif
