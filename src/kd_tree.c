4e34b1691b8cd325464c2625e46b8f5d3361572b
Muzzikon <i.stepanov2@g.nsu.ru>
2026-04-12 00:09:28 +0700

    Довёл KD-дерево до полноценной 3D-реализации и подготовил его к работе с индексами

--- src/kd_tree.c
+++ src/kd_tree.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+
 #include "kd_tree.h"
 
-Node* insert(Node* root, Point point, int depth) {
+Node* insert(Node* root, Point point, int index, int depth) {
     if (root == NULL) {
         Node* new_node = (Node*)malloc(sizeof(Node));
         new_node->point = point;
+        new_node->index = index;
         new_node->left = new_node->right = NULL;
 
         return new_node;
@@ -16,26 +18,26 @@ Node* insert(Node* root, Point point, int depth) {
 
     if (cd == 0) {  // Если ось x
         if (point.x < root->point.x) {
-            root->left = insert(root->left, point, depth + 1);
+            root->left = insert(root->left, point, index, depth + 1);
 		}
 		else {
-            root->right = insert(root->right, point, depth + 1);
+            root->right = insert(root->right, point, index, depth + 1);
         }
     }
 	else if (cd == 1) {  // Если ось y
         if (point.y < root->point.y) {
-            root->left = insert(root->left, point, depth + 1);
+            root->left = insert(root->left, point, index, depth + 1);
         }
 		else {
-            root->right = insert(root->right, point, depth + 1);
+            root->right = insert(root->right, point, index, depth + 1);
         }
     }
     else {
         if (point.z < root->point.z) {
-            root->left = insert(root->left, point, depth + 1);
+            root->left = insert(root->left, point, index, depth + 1);
         }
         else {
-            root->right = insert(root->right, point, depth + 1);
+            root->right = insert(root->right, point, index, depth + 1);
         }
     }
 
@@ -131,13 +133,39 @@ void range_query(Node* root, Point lower, Point upper, int depth, Point* result,
     }
 
     // Рекурсивно ищем в нужных поддеревьях
-    if ((cd == 0 && lower.x < root->point.x) || (cd == 1 && lower.y < root->point.y) || (cd == 2 && lower.z < root->point.z)) {
+    if ((cd == 0 && lower.x <= root->point.x) || (cd == 1 && lower.y <= root->point.y) || (cd == 2 && lower.z <= root->point.z)) {
         range_query(root->left, lower, upper, depth + 1, result, count);
     }
 
-    if ((cd == 0 && upper.x > root->point.x) || (cd == 1 && upper.y > root->point.y) || (cd == 2 && upper.z > root->point.z)) {
+    if ((cd == 0 && upper.x >= root->point.x) || (cd == 1 && upper.y >= root->point.y) || (cd == 2 && upper.z >= root->point.z)) {
         range_query(root->right, lower, upper, depth + 1, result, count);
     }
+}
+
+void range_query_indices(Node* root, Point lower, Point upper, int depth, int* result, int* count) {
+    if (root == NULL) return;
+
+    int cd = depth % 3;
+
+    if (root->point.x >= lower.x && root->point.x <= upper.x &&
+        root->point.y >= lower.y && root->point.y <= upper.y &&
+        root->point.z >= lower.z && root->point.z <= upper.z) {
+        result[*count] = root->index;
+        (*count)++;
+    }
+
+    if ((cd == 0 && lower.x <= root->point.x) || 
+    (cd == 1 && lower.y <= root->point.y) || 
+    (cd == 2 && lower.z <= root->point.z)) {
+        range_query_indices(root->left, lower, upper, depth + 1, result, count);
+    }
+
+    if ((cd == 0 && upper.x >= root->point.x) || 
+    (cd == 1 && upper.y >= root->point.y) || 
+    (cd == 2 && upper.z >= root->point.z)) {
+        range_query_indices(root->right, lower, upper, depth + 1, result, count);
+    }
+
 }
 
 void free_tree(Node* root) {
