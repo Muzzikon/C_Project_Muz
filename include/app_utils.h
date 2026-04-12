dd0b8688b22d7a16c526416a57f15d63fb8d3aa8
Muzzikon <i.stepanov2@g.nsu.ru>
2026-04-12 14:58:58 +0700

    Добавил объявление функции для имени файла диапазонного поиска

--- include/app_utils.h
+++ include/app_utils.h
@@ -5,6 +5,7 @@
 
 int parse_query_point(const char *text, Point *point);
 char *make_dbscan_output_filename(const char *input_filename);
+char *make_range_output_filename(const char *input_filename);
 PointArray load_points_array_from_csv(const char *filename);
 
 Point brute_force_nearest(Point *points, int count, Point target);
@@ -12,5 +13,6 @@ int brute_force_range(Point *points, int count, Point lower, Point upper, Point 
 
 int compare_points(const void *a, const void *b);
 int points_equal(Point *a, Point *b, int count);
+int save_points_csv(const char *filename, Point *points, int count);
 
 #endif
